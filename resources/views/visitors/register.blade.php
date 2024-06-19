efab5e121deae7083d3b537dc1fbefbbf2e59428
DhruvSvt <dhruvsvt08@gmail.com>
2024-06-19 11:13:38 +0530

    Update register.blade.php

--- resources/views/visitors/register.blade.php
+++ resources/views/visitors/register.blade.php
@@ -79,49 +79,8 @@
     <section class="contact-page-section">
         <div class="auto-container">
             <div class="row clearfix">
-                <div class="contact-column col-lg-4 col-md-12 col-sm-12 order-2">
-                    <div class="inner-column">
-                        <div class="sec-title">
-                            <h2>Contact Info</h2>
-                        </div>
-                        <ul class="contact-info">
-                            <li>
-                                <span class="icon fa fa-map-marker-alt"></span>
-                                <p><strong>32, Breaking Street,</strong></p>
-                                <p>2nd cros, Newyork ,USA 10002</p>
-                            </li>
-
-                            <li>
-                                <span class="icon fa fa-phone-volume"></span>
-                                <p><strong>Call Us</strong></p>
-                                <p>+321 4567 89 012 & 79 023</p>
-                            </li>
-
-                            <li>
-                                <span class="icon fa fa-envelope"></span>
-                                <p><strong>Mail Us</strong></p>
-                                <p><a href="mailto:support@example.com">Support@example.com</a></p>
-                            </li>
-
-                            <li>
-                                <span class="icon fa fa-clock"></span>
-                                <p><strong>Opening Time</strong></p>
-                                <p>Mon - Sat: 09.00am to 18.00pm</p>
-                            </li>
-                        </ul>
-
-                        <ul class="social-icon-two social-icon-colored">
-                            <li><a href="#"><i class="fab fa-facebook"></i></a></li>
-                            <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                            <li><a href="#"><i class="fab fa-google-plus"></i></a></li>
-                            <li><a href="#"><i class="fab fa-dribbble"></i></a></li>
-                            <li><a href="#"><i class="fab fa-pinterest"></i></a></li>
-                        </ul>
-                    </div>
-                </div>
-
                 <!-- Form Column -->
-                <div class="form-column col-lg-8 col-md-12 col-sm-12">
+                <div class="form-column col-lg-12 col-md-12 col-sm-12">
                     <div class="inner-column">
                         <div class="contact-form">
                             <div class="sec-title">
